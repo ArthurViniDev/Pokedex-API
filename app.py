cc6b5d3b220ca9507bc8cbca6fbe4fd93af39eb2
ArthurViniDev <kenzygamedev@gmail.com>
2025-03-22 19:17:35 -0300

    some fix

--- app.py
+++ app.py
@@ -10,24 +10,22 @@ pokemons = [
 
 MAX_PAGINATION_LIMIT = 5
 
-# rota de metodo GET para listar os pokemons
-@app.route("/lista", methods=["GET"])
+# Rota GET para listar os Pokémon
+@app.route("/list", methods=["GET"])
 def list_pokemons():
     offset = int(request.args.get("offset", 0))
     limit = int(request.args.get("limit", MAX_PAGINATION_LIMIT))
-
     if limit > MAX_PAGINATION_LIMIT:
         limit = MAX_PAGINATION_LIMIT
-
     return jsonify(pokemons[offset:offset + limit])
 
-# rota para buscar um pokemon pelo nome
-@app.route("/<pokemon_name>", methods=["GET"])
+# Rota GET para buscar um Pokémon pelo nome
+@app.route("/pokemon/<pokemon_name>", methods=["GET"])
 def find_pokemon(pokemon_name):
     for pokemon in pokemons:
-        if pokemon["name"].lower() == pokemon_name:
+        if pokemon["name"].lower() == pokemon_name.lower():
             return jsonify(pokemon)
-    return jsonify({"error" : "pokemon not found"}), 404
+    return jsonify({"error": "Pokémon not found"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
